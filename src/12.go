ec3d107dd5227634ea2b8e4e101a3e2b3adbaf3a
niheufajardo290 <niheufajardo290@gmail.com>
2025-03-18 04:02:16 +0000

    Add 12.go

---
+++ src/12.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Simple experiment design
+func experimentDesign() {
+    fmt.Println("Experiment Design")
+}
+
+// Data analysis for the experiment
+func dataAnalysis() {
+    fmt.Println("Data Analysis")
+}
+
+// Main function
+func main() {
+	experimentDesign()
+	dataAnalysis()
+}
