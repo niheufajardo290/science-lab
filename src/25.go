b868125f2db39a43dd83fcc91997478538fbd2b6
niheufajardo290 <niheufajardo290@gmail.com>
2025-04-05 06:02:52 +0000

    Add 25.go

---
+++ src/25.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var x int = 5
+    if x > 0 {
+        fmt.Println("x is positive")
+    } else {
+        fmt.Println("x is not positive")
+    }
+}
