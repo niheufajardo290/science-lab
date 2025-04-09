7a53558a68dc85085c21d824a5a12139c2cbf31e
niheufajardo290 <niheufajardo290@gmail.com>
2025-04-09 20:39:58 +0000

    Add 29.go

---
+++ src/29.go
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] data = {{1, 2}, {3, 4}, {5, 6}};
+        
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(Arrays.toString(data[i]));
+        }
+    }
+}
